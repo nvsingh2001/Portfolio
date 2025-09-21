a5519496db6b53d0b26cada5fcea7ce17d4b66e1
nvsingh2001 <nvsingh2001@hotmail.com>
2025-09-21 19:23:28 +0530

    feat: add neons effect and update styles
    - Added neons.scss for glowing text effects.
    - Imported neons.scss in main.js.
    - Updated font weight for city-night-light in style.css.
    - Added badge styles in style.css.
    - Added Bootstrap.svg and suitcase.svg assets.
    - Added sass-embedded as a dev dependency in package.json.

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "./background.css";
 import "./border.css";
+import "./neons.scss";
 
 document.getElementById("year").innerHTML = new Date().getFullYear();
 
